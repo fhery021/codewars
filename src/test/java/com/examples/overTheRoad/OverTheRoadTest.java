f52f527ae12df259f7ad778dd6ee6a94da629d88
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-06-21 15:04:24 +0200

    Replaced obsolete assertEquals

--- src/test/java/com/examples/overTheRoad/OverTheRoadTest.java
+++ src/test/java/com/examples/overTheRoad/OverTheRoadTest.java
@@ -1,6 +1,7 @@
 package com.examples.overTheRoad;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -9,20 +10,20 @@ class OverTheRoadTest {
 
     @Test
     public void smallStreetTest() {
-        assertEquals(4, OverTheRoad.overTheRoad(1, 2));
-        assertEquals(2, OverTheRoad.overTheRoad(3, 2));
-        assertEquals(1, OverTheRoad.overTheRoad(4, 2));
-        assertEquals(3, OverTheRoad.overTheRoad(2, 2));
+        Assertions.assertEquals(4, OverTheRoad.overTheRoad(1, 2));
+        Assertions.assertEquals(2, OverTheRoad.overTheRoad(3, 2));
+        Assertions.assertEquals(1, OverTheRoad.overTheRoad(4, 2));
+        Assertions.assertEquals(3, OverTheRoad.overTheRoad(2, 2));
     }
 
     @Test
     public void basicTestCases() {
-        assertEquals(6, OverTheRoad.overTheRoad(1, 3));
-        assertEquals(4, OverTheRoad.overTheRoad(3, 3));
-        assertEquals(5, OverTheRoad.overTheRoad(2, 3));
-        assertEquals(8, OverTheRoad.overTheRoad(3, 5));
-        assertEquals(16, OverTheRoad.overTheRoad(7, 11));
-        assertEquals(1999981L, OverTheRoad.overTheRoad(20, 1000000));
-        assertEquals(596421736780L, OverTheRoad.overTheRoad(23633656673L, 310027696726L));
+        Assertions.assertEquals(6, OverTheRoad.overTheRoad(1, 3));
+        Assertions.assertEquals(4, OverTheRoad.overTheRoad(3, 3));
+        Assertions.assertEquals(5, OverTheRoad.overTheRoad(2, 3));
+        Assertions.assertEquals(8, OverTheRoad.overTheRoad(3, 5));
+        Assertions.assertEquals(16, OverTheRoad.overTheRoad(7, 11));
+        Assertions.assertEquals(1999981L, OverTheRoad.overTheRoad(20, 1000000));
+        Assertions.assertEquals(596421736780L, OverTheRoad.overTheRoad(23633656673L, 310027696726L));
     }
 }
