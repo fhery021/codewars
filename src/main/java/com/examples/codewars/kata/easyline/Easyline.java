bda1c84eeda9976214a19bab3d5e300be5b05dfd
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-06-23 11:13:34 +0200

    nominal case ok

--- src/main/java/com/examples/codewars/kata/easyline/Easyline.java
+++ src/main/java/com/examples/codewars/kata/easyline/Easyline.java
@@ -2,14 +2,27 @@ package com.examples.codewars.kata.easyline;
 
 import java.math.BigInteger;
 
+/**
+ * We want to calculate the sum of the squares of the binomial coefficients on a given horizontal line with a function called easyline (or easyLine or easy-line).
+ * <p>
+ * Can you write a program which calculate easyline(n) where n is the horizontal line number?
+ * <p>
+ * The function will take n (with: n>= 0) as parameter and will return the sum of the squares of the binomial coefficients on line n.
+ */
 public class Easyline {
-    public static BigInteger easyLine(int i) {
-        if (i == 0) {
-            return new BigInteger("1");
+
+
+    private static BigInteger calculate(int n, int p) {
+        BigInteger ret = BigInteger.ONE;
+        for (int i = 0; i < p; i++) {
+            ret = ret.multiply(BigInteger.valueOf(n - i))
+                    .divide(BigInteger.valueOf(i + 1));
         }
-        if (i == 1) {
-            return new BigInteger("2");
-        }
-        return null;
+        return ret;
     }
+
+    public static BigInteger easyLine(int n) {
+        return calculate(2 * n, n);
+    }
+
 }
--- src/test/java/com/examples/codewars/kata/easyline/EasyLineTest.java
+++ src/test/java/com/examples/codewars/kata/easyline/EasyLineTest.java
@@ -15,6 +15,7 @@ class EasyLineTest {
     @Test
     public void test1() {
         testing(Easyline.easyLine(7),  new BigInteger("3432"));
+        testing(Easyline.easyLine(4),  new BigInteger("70"));
         testing(Easyline.easyLine(13), new BigInteger("10400600"));
         testing(Easyline.easyLine(17), new BigInteger("2333606220"));
         testing(Easyline.easyLine(19), new BigInteger("35345263800"));
