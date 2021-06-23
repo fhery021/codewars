e311e9574e109a263492ccd370408166d4a08b91
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-06-23 16:50:07 +0200

    highest and lowest kata

--- src/main/java/com/examples/codewars/kata/highestandlowest/Kata.java
+++ src/main/java/com/examples/codewars/kata/highestandlowest/Kata.java
@@ -1,8 +1,19 @@
 package com.examples.codewars.kata.highestandlowest;
 
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+
 public class Kata {
     public static String highAndLow(String numbers) {
-        // Code here or
-        return "throw towel";
+
+        IntSummaryStatistics statistics = Arrays.stream(numbers.split(" "))
+                .mapToInt(Integer::valueOf)
+                .summaryStatistics();
+
+        return String.format("%d %d", statistics.getMax(), statistics.getMin());
     }
+
+
+
+
 }
--- src/test/java/com/examples/codewars/kata/highestandlowest/KataTest.java
+++ src/test/java/com/examples/codewars/kata/highestandlowest/KataTest.java
@@ -10,4 +10,9 @@ class KataTest {
     public void test1() {
         assertEquals("42 -9", Kata.highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
     }
+
+    @Test
+    public void sameValuesTest() {
+        assertEquals("1 1", Kata.highAndLow("1"));
+    }
 }
