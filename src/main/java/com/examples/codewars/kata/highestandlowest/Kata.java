143adc1e93374a136c736e3c0ca47bba066da82d
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-06-23 11:18:03 +0200

    highest and lowest kata

---
+++ src/main/java/com/examples/codewars/kata/highestandlowest/Kata.java
@@ -0,0 +1,8 @@
+package com.examples.codewars.kata.highestandlowest;
+
+public class Kata {
+    public static String highAndLow(String numbers) {
+        // Code here or
+        return "throw towel";
+    }
+}
