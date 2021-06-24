593293bc70de1b0b0d5e4c92bbbf964f6736e5de
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-06-24 16:25:39 +0200

    Find the squares ok

--- src/main/java/com/examples/codewars/kata/findthesquares/Solution.java
+++ src/main/java/com/examples/codewars/kata/findthesquares/Solution.java
@@ -1,7 +1,10 @@
 package com.examples.codewars.kata.findthesquares;
 
-public class Solution{
-    public static String findSquares(int n){
-     return "1-0";
+public class Solution {
+    public static String findSquares(int n) {
+        long bigger = Math.round(Math.pow(Math.ceil((float) n / 2), 2));
+        long smaller = Math.round(Math.pow(Math.floor((float) n / 2), 2));
+
+        return String.format("%d-%d", bigger, smaller);
     }
 }
--- src/test/java/com/examples/codewars/kata/findthesquares/SolutionTest.java
+++ src/test/java/com/examples/codewars/kata/findthesquares/SolutionTest.java
@@ -5,19 +5,20 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionTest {
-//    @Test
-//    public void testBasicNumbers() {
-//        assertEquals("25-16", Solution.findSquares(9));
-//    }
+
+    @Test
+    public void testBasicNumbers() {
+        assertEquals("25-16", Solution.findSquares(9));
+    }
 
     @Test
     public void testSmallerNumbers() {
         assertEquals("1-0", Solution.findSquares(1));
     }
-//
-//    @Test
-//    public void testBiggerNumbers() {
-//        assertEquals("891136-889249", Solution.findSquares(1887));
-//        assertEquals("2499600016-2499500025", Solution.findSquares(99991));
-//    }
+
+    @Test
+    public void testBiggerNumbers() {
+        assertEquals("891136-889249", Solution.findSquares(1887));
+        assertEquals("2499600016-2499500025", Solution.findSquares(99991));
+    }
 }
