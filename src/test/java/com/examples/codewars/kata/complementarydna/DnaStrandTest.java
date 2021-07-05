9176b132f0ee3252fb5ed8c78a84a6d5909a4cd7
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-07-05 15:43:39 +0200

    DnaStrandTest AAAA test

--- src/test/java/com/examples/codewars/kata/complementarydna/DnaStrandTest.java
+++ src/test/java/com/examples/codewars/kata/complementarydna/DnaStrandTest.java
@@ -6,10 +6,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class DnaStrandTest {
 
-//    @Test
-//    public void test01() {
-//        assertEquals("TTTT", DnaStrand.makeComplement("AAAA"));
-//    }
+    @Test
+    public void test01() {
+        assertEquals("TTTT", DnaStrand.makeComplement("AAAA"));
+    }
 //    @Test
 //    public void test02() {
 //        assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
@@ -18,4 +18,5 @@ class DnaStrandTest {
 //    public void test03() {
 //        assertEquals("CATA", DnaStrand.makeComplement("GTAT"));
 //    }
+
 }
