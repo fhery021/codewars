8f25a89c78ad332b74a2ae3dc06d5ebd81c5e046
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-07-05 16:11:12 +0200

    DnaStrandTest

--- src/main/java/com/examples/codewars/kata/complementarydna/DnaStrand.java
+++ src/main/java/com/examples/codewars/kata/complementarydna/DnaStrand.java
@@ -1,8 +1,30 @@
 package com.examples.codewars.kata.complementarydna;
 
+import java.util.Map;
+
 public class DnaStrand {
 
     public static String makeComplement(String dna) {
-        return "TTTT";
+        return dna.toUpperCase()
+                .chars()
+                .map(c -> complementOf((char) c))
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+
+    private static char complementOf(char c) {
+        switch (c) {
+            case 'T':
+                return 'A';
+            case 'A':
+                return 'T';
+            case 'C':
+                return 'G';
+            case 'G':
+                return 'C';
+            default:
+                return 'X';
+        }
     }
 }
--- src/test/java/com/examples/codewars/kata/complementarydna/DnaStrandTest.java
+++ src/test/java/com/examples/codewars/kata/complementarydna/DnaStrandTest.java
@@ -10,14 +10,15 @@ class DnaStrandTest {
     public void test01() {
         assertEquals("TTTT", DnaStrand.makeComplement("AAAA"));
     }
-//    @Test
-//    public void test02() {
-//        assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
-//    }
 
-    //    @Test
-//    public void test03() {
-//        assertEquals("CATA", DnaStrand.makeComplement("GTAT"));
-//    }
+    @Test
+    public void test02() {
+        assertEquals("TAACG", DnaStrand.makeComplement("ATTGC"));
+    }
+
+    @Test
+    public void test03() {
+        assertEquals("CATA", DnaStrand.makeComplement("GTAT"));
+    }
 
 }
