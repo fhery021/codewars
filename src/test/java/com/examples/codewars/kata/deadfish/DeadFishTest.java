7bb79903ad27d2df9370fde028acd70be158c64b
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-07-08 11:27:02 +0200

    DeadFish red

--- src/test/java/com/examples/codewars/kata/deadfish/DeadFishTest.java
+++ src/test/java/com/examples/codewars/kata/deadfish/DeadFishTest.java
@@ -1,7 +1,15 @@
 package com.examples.codewars.kata.deadfish;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeadFishTest {
+
+    @Test
+    public void exampleTest() {
+        assertArrayEquals(new int[] {8, 64}, DeadFish.parse("iiisdoso"));
+
+    }
 
 }
