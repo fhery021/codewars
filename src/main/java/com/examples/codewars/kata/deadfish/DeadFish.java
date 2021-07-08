9ac92e2ee63e41a2c8e38d5780e94120acf86a6a
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-07-08 11:25:22 +0200

    DeadFish 0

---
+++ src/main/java/com/examples/codewars/kata/deadfish/DeadFish.java
@@ -0,0 +1,9 @@
+package com.examples.codewars.kata.deadfish;
+
+public class DeadFish {
+    public static int[] parse(String data) {
+        // Implement me! :)
+        int [] solution = new int[0];
+        return solution;
+    }
+}
