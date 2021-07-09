0ab43cff6c5936d924aef8e23d4bf7d7a7fb42fb
Ferenc Bondár <ferenc.bondar@ibm.com>
2021-07-09 10:13:41 +0200

    DRoot third green

--- src/main/java/com/examples/codewars/kata/digitalroot/DRoot.java
+++ src/main/java/com/examples/codewars/kata/digitalroot/DRoot.java
@@ -1,18 +1,31 @@
 package com.examples.codewars.kata.digitalroot;
 
+import java.util.List;
+
 /**
  * Digital root is the recursive sum of all the digits in a number.
- *
+ * <p>
  * Given n, take the sum of the digits of n.
  * If that value has more than one digit, continue reducing in this way until a single-digit number is produced.
- *
+ * <p>
  * The input will be a non-negative integer.
  */
 public class DRoot {
 
-
+    private static int sumOfDigits(int n) {
+        return String.valueOf(n)
+                .chars()
+                .map(Character::getNumericValue)
+                .sum();
+    }
 
     public static int digital_root(int n) {
-        return 6;
+        int sum = sumOfDigits(n);
+        if (sum < 10) {
+            return sum;
+        } else {
+            sum = sumOfDigits(sum);
+        }
+        return sum;
     }
 }
